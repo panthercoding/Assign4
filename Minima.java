a18e8474df393c41fe5c48f1c72e703172bd268d
panthercoding <79004908+panthercoding@users.noreply.github.com>
2021-03-08 23:20:45 -0600

    Create Minima.java

---
+++ Minima.java
@@ -0,0 +1,25 @@
+public class Minima{
+
+ /* @param three integer numbers
+ @return the minimum of the three integers */
+ 
+  public static int minimum(int a, int b, int c){
+    if ((a <= b) && (b <= c)){
+      return(a + " " + b + " " + c);
+    }
+    else if ((a <= b) && (b <= c)){
+
+    }
+    else if ()
+  }
+
+  /* main method */
+  public static void main(String[] args){
+    int a = Integer.parseInt(args[0]);
+    int b = Integer.parseInt(args[1]);
+    int c = Integer.parseInt(args[2]);
+    System.out.println(sort(a,b,c));
+
+  }
+
+}
